c52836dea7f292a4d0da966eb79b615ba22c3f54
britcorona <BrittneyLCorona@gmail.com>
2015-11-20 12:17:21 -0600

    JitterContext

---
+++ Jitter/Models/JitterContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity; //This allows you to inherit
+
+namespace Jitter.Models
+{
+    public class JitterContext : DbContext //This now inherits from a Db (database). Uses default connections.
+    {
+        public DbSet<JitterUser> JitterUsers { get; set; } //This creates the table in the database
+        public DbSet<Jot> Jots { get; set; } //Way into the database.
+    }
+}
