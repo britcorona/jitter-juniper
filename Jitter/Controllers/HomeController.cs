14a248f1235f98cc09c589b08e3fc7268209192f
britcorona <BrittneyLCorona@gmail.com>
2015-12-03 12:06:04 -0600

    Made some minor changes to homecontroller for learning purposes

--- Jitter/Controllers/HomeController.cs
+++ Jitter/Controllers/HomeController.cs
@@ -10,14 +10,22 @@ namespace Jitter.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View(); //dec_3 branch. View is a thing called View Bag, kind of like a grocery bag, where this stuff is put in here when its returned. Go to Views folder, find the home subfolder and look at the about file and you will see where they use @ViewBag.
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
 
-            return View();
+            //1.Create or Get a list of things
+            List<string> my_list_of_things = new List<string>();
+            my_list_of_things.Add("Timmy");
+            my_list_of_things.Add("Chef");
+            my_list_of_things.Add("Greg");
+
+            //One way to send the my_list_things to the view is: return View(my_list_of_things);
+
+            return View(my_list_of_things);
         }
 
         public ActionResult Contact()
